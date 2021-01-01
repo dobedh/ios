d34784e1df0ab7295f128c4bd34bdc0f2b81aeae
김동현 <dobedh@gmail.com>
2021-01-01 12:31:44 +0900

    Initial Commit

---
+++ Part 1 - (ALL EPISODES) Data Types & Operators - Begin.playground/Pages/02 - Xcode Playgrounds & Comments.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [⇐ Previous: 01 - Introduction](@previous)
+//: ## Episode 02: Xcode Playgrounds
+import Foundation
+
+var welcomeMessage = "Hello, playground"
+
+let goodbyeMessage = "good bye then,"
+
+// cooment type 1
+/* comment type 2 */
+
+print(goodbyeMessage)
+
+//print(goodbyeMessage)
+//: [⇒ Next: 03 - Booleans & Comparison Operators](@next)
+
---
+++ Part 1 - (ALL EPISODES) Data Types & Operators - Begin.playground/Pages/03 - Booleans & Comparison Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [⇐ Previous: 02 - Xcode Playgrounds & Comments](@previous)
+//: ## Episode 03: Booleans & Comparison Operators
+//let yes: Bool = true
+let yes = true
+
+//let no: Bool = false
+let no = false
+
+let passingGrade = 50
+let studentGrade = 50
+
+//let studnetPassed = studentGrade > passingGrade
+let studentPassed = studentGrade >= passingGrade
+
+let studentNotPassed = studentGrade < passingGrade
